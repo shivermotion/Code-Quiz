288382facc10c7c0e3f1fdf052c727527c5576c1
Jason Day <jason.day.blue@gmail.com>
2021-11-14 23:27:17 -0600

    timer completed-questions code still updating

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 //descending timer
 let timer = function () {
 	let sec = 10;
+
 	let interval = setInterval(function () {
 		document.getElementById("timer").textContent =
 			sec + " sec remaining";
@@ -17,7 +18,7 @@ let timer = function () {
 };
 
 //scoring
-let newScore = sec;
+// let newScore = sec;
 
 //scoring and highscore bank
 // let highScoreBank = [];
@@ -26,18 +27,26 @@ let newScore = sec;
 // }
 
 //questionsboard
-questionbank = [
-	"what does 2 + 2 = ?",
-	"what does 7 + 6 = ?",
-	"what does 2 + 7 = ?",
-	"what does 0 + 9 = ?",
-	"what does 2 + 1 = ?",
-	"what does 4 + 4 = ?",
-	"what does 6 + 5 = ?",
-	"what does 8 + 2 = ?",
-	"what does 1 + 5 = ?",
-	"what does 1 + 1 = ?",
-];
+let game = function () {
+	let questionBank = [
+		"what does 2 + 2 = ?",
+		"what does 7 + 6 = ?",
+		"what does 2 + 7 = ?",
+		"what does 0 + 9 = ?",
+		"what does 2 + 1 = ?",
+		"what does 4 + 4 = ?",
+		"what does 6 + 5 = ?",
+		"what does 8 + 2 = ?",
+		"what does 1 + 5 = ?",
+		"what does 1 + 1 = ?",
+	];
+	document.querySelector("section").textContent = questionBank[0];
+	console.log(questionBank[0]);
+};
+
+// let answerBank []
+// let question = questionBank[(Math.random() * questionBank.length) | 0];
+// console.log(question);
 
 //buttons
 
