a73679861531f020b4f372ad0b37dd5e541c081f
Jason Day <jason.day.blue@gmail.com>
2021-11-15 23:51:45 -0600

    save

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 //game-question and matching answers
 let game = function () {
+	// score
+	totalScore = 0;
+
 	let questionBank = [
 		"what does 2 + 2 = ?",
 		"what does 7 + 6 = ?",
@@ -12,21 +15,45 @@ let game = function () {
 		"what does 1 + 5 = ?",
 		"what does 1 + 1 = ?",
 	];
+
 	let answerBank = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-	///
-	document.querySelector("section").textContent = questionBank[0];
+
+	// question board
+	let questionBoard = document.querySelector("section");
 	console.log(questionBank[0]);
-	///
-	document.querySelector("#choice1").textContent = answerBank[1];
-	document.querySelector("#choice2").textContent = answerBank[2];
-	document.querySelector("#choice3").textContent = answerBank[5];
-	document.querySelector("#choice4").textContent = answerBank[6];
-	console.log(answerBank[6]);
+	// buttons
+	choice1 = document.querySelector("#choice1");
+	choice2 = document.querySelector("#choice2");
+	choice3 = document.querySelector("#choice3");
+	choice4 = document.querySelector("#choice4");
+
+	// question 1
+	questionBoard.textContent = questionBank[0];
+	console.log(questionBoard);
+	//choices
+	choice1.textContent = answerBank[0];
+	choice2.textContent = answerBank[1];
+	choice3.textContent = answerBank[5];
+	// correct answer
+	choice4.textContent = answerBank[4];
+
+	// if user picks correct answer
+	choice4.onclick = function () {
+		totalScore++;
+		console.log(totalScore);
+		// question 2
+		questionBoard.textContent = questionBank[1];
+		// choices
+		choice1.textContent = answerBank[13];
+		choice2.textContent = answerBank[4];
+		choice3.textContent = answerBank[8];
+		choice4.textContent = answerBank[1];
+	};
 };
 
 //descending timer
 let timer = function () {
-	let sec = 10;
+	let sec = 99;
 	let interval = setInterval(function () {
 		document.getElementById("timer").textContent =
 			sec + " sec remaining";
